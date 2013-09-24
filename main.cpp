dea22a9fef3b5900282a267bf1ae3718aa3146ee
Dmitrij Lioubartsev <tinker234@hotmail.com>
2013-09-24 13:24:33 +0200

    lagt till main

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char **argv) {
+	
+	// Read the board
+	vector<string> board;
+	int width = 0;
+	int height = 0;
+	for (string line; getline(cin, line);) {
+		board.push_back(line);
+		if(line.length > width)
+			width = line.length();
+	}
+	height = board.size();
+
+	// Create gamestate
+
+	//GameState
+	string s = "U R R U";
+
+	// Output answer
+	std::cout << s << std::endl;
+	return 0;
+}
