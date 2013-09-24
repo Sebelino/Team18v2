bd2aac63c5cf705961781acb800af45ea1b12231
Dmitrij Lioubartsev <tinker234@hotmail.com>
2013-09-24 13:45:31 +0200

    stadat upp headern

--- gamestate.h
+++ gamestate.h
@@ -5,8 +5,12 @@
 class GameState{
 public:
     GameState(vector<string> stringmap,int width,int height);
+	GameState(vector<vector<char> > stringmap,int width,int height);
     ~GameState();
+
     vector<vector<char> > map;
+	GameState pushBox(const struct move& m);
+
     int playerX;
     int playerY;
 };
