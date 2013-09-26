bda8f4faefc598c983fa884efd481c30597ad26e
Dmitrij Lioubartsev <tinker234@hotmail.com>
2013-09-26 19:33:02 +0200

    inget egentligen

--- AI.cpp
+++ AI.cpp
@@ -1,13 +1,23 @@
 //include stuff
 //#include <cstdio>
 //#include <cstdlib>
+#include "GameState.h"
+#include "Map.h"
+#include <queue>
+
+
 
 
 #ifndef _AI_CPP__
 #define _AI_CPP__
+
+//class Heuristic;
+
 //TODO Make sense of this function
-int solve_pas(void) {
-	return 42;
+void solve(Map* m, GameState start);
+
+void solve(Map * m, GameState start) {
+	
 }
 //test
 #endif
