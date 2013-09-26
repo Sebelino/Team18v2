b9f730523f51e715ef99009636245750692cacb5
Sebelino <sebelino@hotmail.com>
2013-09-26 19:24:42 +0200

    Headerfil för AI.

--- AI.h
+++ AI.h
@@ -2,8 +2,9 @@
 //#include <cstdio>
 //#include <cstdlib>
 
+#ifndef _AI_H__
+#define _AI_H__
 //TODO Make sense of this function
-int solve_pas(void) {
-	return 42;
-}
+int solve_pas(void);
 //test
+#endif
