e8b6f00fdd28bfed560864e2b8978ff11ff42487
Kricka <kristoffer.hallqvist@gmail.com>
2013-09-26 19:25:31 +0200

    ..

--- AI.cpp
+++ AI.cpp
@@ -1,13 +1,51 @@
-//include stuff
-//#include <cstdio>
-//#include <cstdlib>
-
-
 #ifndef _AI_CPP__
 #define _AI_CPP__
-//TODO Make sense of this function
-int solve_pas(void) {
-	return 42;
+
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include "GameState.h"
+
+class GameState;
+
+void solve(GameState gs) {
+	return;
 }
-//test
+
+int abprune(GameState curGS, int depth, int a, int b, bool maxi)
+{
+    if (depth == 0) { // || curGS.isEOG()) {
+        //fprintf(stderr, "Entered first if. In abprune now with depth %i\n", depth);
+        //if (curGS.isDraw()) {
+        //    return 0;
+        //}
+        return 0;
+        //return evalHeur(curGS);
+    }
+ 
+    //fprintf(stderr, "In abprune now with depth %i\n", depth);
+ 
+    std::vector<GameState> lNextStates;
+    curGS.findNextMoves(lNextStates);
+    int vecSize = lNextStates.size();
+ 
+    if (maxi) { //Maximizing player's turn
+        for (int i = 0;i<vecSize;i++) {
+            a = std::max(a, abprune(lNextStates[i], depth-1, a, b, !maxi));
+            if (b <= a) {
+                break;
+            }
+        }
+        return a;
+    } else {    //Minimizing player's turn
+        for (int i = 0;i<vecSize;i++) {
+            b = std::min(b, abprune(lNextStates[i], depth-1, a, b, !maxi));
+            if (b <= a) {
+                break;
+            }
+        }
+        return b;
+    }
+}
+
 #endif
