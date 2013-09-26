78b635559100b69f586b207158094635ea127ab4
Kricka <kristoffer.hallqvist@gmail.com>
2013-09-26 23:35:26 +0200

    segfault..

--- Main.cpp
+++ Main.cpp
@@ -43,19 +43,26 @@ int main(int argc, char **argv) {
 	string s = "U R R U";
 	vector<char> str;
 	
+	fprintf(stderr,"0\n");
+	
 	vector<GameState> solution = solve(&gs);
+	
+	fprintf(stderr,"1\n");
+	
 	vector<string> movements(solution.size());
 	
-	map.findStaticDeadLocks();
+	fprintf(stderr,"2\n");
 
 	// Output answer
-	for (int i = 0;i<solution.size(); i++) {
+	for (int i = 0;i<solution.size()-1; i++) {
 		str = moveToPath(&(solution[i]),solution[i+1].src);
 		for (int j = 0;j<str.size(); j++) {
 			printf("%c",str[j]);
 		}
 	}
 	putchar('\n');
+	
+	fprintf(stderr,"3\n");
 
 	return 0;
 }
